821a41b466047a1850b05c46226a7da5848b8036
Jonathan Yantis <yantisj@Jonathans-iMac.local>
2016-07-02 11:03:07 -0400

    Fixed pytest-3 glitch

--- test/test_full.sh
+++ test/test_full.sh
@@ -1,8 +1,8 @@
 #/bin/sh
-export pytestcmd="py.test"
+export pytestcmd="py.test-3"
 
 if ! type "$pytestcmd" 2> /dev/null; then
-    export pytestcmd="py.test-3"
+    export pytestcmd="py.test"
 fi
 
 $pytestcmd --resultlog=/tmp/pytest.log ngtest.py
