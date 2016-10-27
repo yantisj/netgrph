40b81a9f7459cf894dc77343548a11c8809e4014
NetGrph User <netgrph@hal.musc.edu>
2016-10-27 15:30:04 -0400

    Updated production to run from netgrph user

--- datasources/update_csv_from_netdb.sh
+++ datasources/update_csv_from_netdb.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-# Change to the current folder, wherever that is
-cd /home/yantisj/netgrph/dev/datasources/
-./netdb-nd.py -nf /opt/netdb/data/nd.csv -of /home/yantisj/csv/nd.csv
-./netdb-devfile.py -df /scripts/devicelist.csv -of /home/yantisj/csv/devices.csv
+# Change to the current netgrph folder
+export NGHOME=/home/netgrph/netgrph
+export NGCSV=/home/netgrph/csv
+export NETDBDATA=/opt/netdb/data
+
+cd $NGHOME/datasources/
+./netdb-nd.py -nf $NETDBDATA/nd.csv -of $NGCSV/nd.csv
+./netdb-devfile.py -df $NETDBDATA/devicelist.csv -of $NGCSV/devices.csv
--- datasources/update_parse.sh
+++ datasources/update_parse.sh
@@ -1,3 +1,7 @@
 #/bin/sh
-/home/yantisj/netgrph/prod/datasources/ciscoparse.py -vr 1-4096 -ivr 0-4096 -df /home/yantisj/csv/devices.csv -dfile /home/yantisj/csv/devinfo.csv -mf /scripts/versions.txt -lfile /home/yantisj/csv/links.csv -ifile /home/yantisj/csv/allnets.csv -vfile /home/yantisj/csv/allvlans.csv
-/home/yantisj/netgrph/dev/datasources/asaparse.py -fd ~/csv/asafirewalls.csv -debug 0 -ffile /home/yantisj/csv/firewalls.csv
+export NGHOME=/home/netgrph/netgrph
+export NGCSV=/home/netgrph/csv
+export NETDBDATA=/opt/netdb/data
+
+$NGHOME/datasources/ciscoparse.py -vr 1-4096 -ivr 0-4096 -df $NGCSV/devices.csv -dfile $NGCSV/devinfo.csv -mf /scripts/versions.txt -lfile $NGCSV/links.csv -ifile $NGCSV/allnets.csv -vfile $NGCSV/allvlans.csv
+$NGHOME/datasources/asaparse.py -fd $NGCSV/asafirewalls.csv -debug 0 -ffile $NGCSV/firewalls.csv
